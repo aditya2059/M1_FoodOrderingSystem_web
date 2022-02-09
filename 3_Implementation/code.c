b40e09d0532227fc8a0ed805f93f2e0667636e3c
aditya2059 <adityakrsingh3344@gmail.com>
2022-02-09 16:00:48 +0530

    done implemenetation

--- 3_Implementation/code.c
+++ 3_Implementation/code.c
@@ -1,9 +1,9 @@
 /**
  * @file code.c
- * @author your name (aditya kumar)
+ * @author your name (you@domain.com)
  * @brief 
  * @version 0.1
- * @date 2022-02-07
+ * @date 2022-02-09
  * 
  * @copyright Copyright (c) 2022
  * 
---
+++ 3_Implementation/code.h
@@ -0,0 +1,12 @@
+/**
+ * @file code.h
+ * @author your name (you@domain.com)
+ * @brief ordering food online
+ * @version 0.1
+ * @date 2022-02-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// function prototype
